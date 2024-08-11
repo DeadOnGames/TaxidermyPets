d884f7b9e8de2f5a70c86deec42dff0f8e44e0b8
DeadOnGames <deadongamesonline@gmail.com>
2024-08-11 18:33:07 +0100

    Fix navigation buttons

--- Assets/Scripts/NextScreenController.cs
+++ Assets/Scripts/NextScreenController.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +10,41 @@ public class NextScreenController : MonoBehaviour
     private int numberOfStations;
     private GameManager gameManager;
 
+    private List<int> stations = new List<int>();
+    private int currentStation;
+
+    public GameObject rightArrow, leftArrow;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.Instance;
         mainCamera = Camera.main;
         numberOfStations = gameManager.NumberOfStations;
+
+        for (int i = 0; i < numberOfStations; i++)
+        {
+            stations.Add(i);
+        }
+
+        currentStation = 0;
+        leftArrow.SetActive(false);
     }
 
     public void MoveRight()
     {
         mainCamera.transform.Translate(15, 0, 0);
+        currentStation = stations[currentStation + 1];
+        if (currentStation == stations[stations.Count - 1]) rightArrow.SetActive(false);
+        if (currentStation != stations[0]) leftArrow.SetActive(true);
+
     }
+
     public void MoveLeft()
     {
         mainCamera.transform.Translate(-15, 0, 0);
+        currentStation = stations[currentStation - 1];
+        if (currentStation == stations[0]) leftArrow.SetActive(false);
+        if (currentStation != stations[stations.Count - 1]) rightArrow.SetActive(true);
     }
 }
