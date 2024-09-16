29ee482977578ac2e59eed5a43eabd99d5c66649
DeadOnGames <deadongamesonline@gmail.com>
2024-09-16 20:55:17 +0100

    Add teleport animation

--- Assets/Scripts/SnapPointsScripts/TeleporterSnapPoint.cs
+++ Assets/Scripts/SnapPointsScripts/TeleporterSnapPoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TeleporterSnapPoint : SnapPoint
 {
     public SnapPoint teleportDestination;
+    public float TeleportDuration = 2.0f;
 
     public override void Snap(Draggable objectToSnap)
     {
@@ -18,8 +19,23 @@ public class TeleporterSnapPoint : SnapPoint
 
     private void TeleportObject(Draggable objectToTeleport)
     {
-        objectToTeleport.transform.position = teleportDestination.transform.position;
-        // Additional logic for teleportation
+        StartCoroutine(MoveToPosition(objectToTeleport, teleportDestination.transform.position, TeleportDuration));
     }
+
+    private IEnumerator MoveToPosition(Draggable objectToMove, Vector3 targetPosition, float duration)
+    {
+        Vector3 startPosition = objectToMove.transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            objectToMove.transform.position = Vector3.Lerp(startPosition, startPosition + new Vector3(10,0,0), elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null; //Wait until next frame
+        }
+
+        objectToMove.transform.position = targetPosition;
+    }
+
 }
 
