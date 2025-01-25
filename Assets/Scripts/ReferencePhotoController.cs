043dbdaa4630c941463f31c66e62061d6efdb105
DeadOnGames <deadongamesonline@gmail.com>
2025-01-25 21:47:09 +0000

    Add cards to game and tweak positioning

--- Assets/Scripts/ReferencePhotoController.cs
+++ Assets/Scripts/ReferencePhotoController.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class ReferencePhotoController : MonoBehaviour
 {
     private Vector3 originalScale;
+    private Vector3 originalPosition;
+
     private bool isExpanded;
     public float scaleMultiplier = 2;
 
     private void Start()
     {
         originalScale = transform.localScale;
+        originalPosition = transform.localPosition;
     }
     public void ExpandPhoto()
     {
         if (!isExpanded)
         {
             transform.localScale *= scaleMultiplier;
+            transform.localPosition = originalPosition + new Vector3(30,-30,0);
             isExpanded = true;
         }
         else
         {
             transform.localScale = originalScale;
+            transform.localPosition = originalPosition;
             isExpanded = false;
         }
         
